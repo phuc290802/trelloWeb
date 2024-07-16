3949e42e035d3b5afef7cb4a096653e7b519b16a
phuc290802 <130070911+phuc290802@users.noreply.github.com>
2024-07-16 11:01:10 +0700

    Refactor project Structure.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
