8a00045de5283f763319dc7d70c2f6bc3136d96d
phuc290802 <130070911+phuc290802@users.noreply.github.com>
2024-07-23 23:12:20 +0700

    Add api to drag card and column

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client'
 import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from '~/theme'
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
+      <ToastContainer />
     </CssVarsProvider>
 
   </React.StrictMode>
